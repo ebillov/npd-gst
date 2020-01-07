1d0fe84ca7d94374d970fed46ba4d7078335feec
ebillov <ebillov@gmail.com>
2020-01-07 18:24:24 +0800

    Partially completed form.

--- Classes/NPD_GST_Admin_Menu.php
+++ Classes/NPD_GST_Admin_Menu.php
@@ -28,26 +28,102 @@ class NPD_GST_Admin_Menu {
      */
     public static function render_settings(){
         $tabs = [
-            'General',
-            'GST',
-            'PDF Invoice'
+            'General' => self::general_settings(),
+            'GST' => self::gst_settings()
         ];
-        
+        include_once NPD_GST_DIR_PATH . 'views/settings.php';
     }
 
     /**
      * General tab settings fields
      * @return array
      */
-    public static function general(){
+    public static function general_settings(){
 
         return [
-            'text' => [
-                'label' => ''
+            'checkbox' => [
+                'label' => 'Include QR Code On Invoices',
+                'name' => 'enable_qr_code',
+                'value' => '',
+                'description' => 'Enable to display the QR code of the invoices.'
+            ],
+            'submit' => [
+                'name' => 'general_save_settings',
+                'value' => 'Save General Settings'
             ]
         ];
 
     }
 
+    /**
+     * GST tab settings fields
+     * @return array
+     */
+    public static function gst_settings(){
+
+        return [
+            'text' => [
+                'label' => 'GSTIN Number',
+                'name' => 'gstin_number',
+                'value' => '',
+                'description' => 'The GSTIN Number to be displayed on invoices.'
+            ],
+            'text' => [
+                'label' => 'GST Rate',
+                'name' => 'gst_rate',
+                'value' => '',
+                'description' => 'The GST tax rate to be included in the cart, checkout details and order invoices.'
+            ],
+            'submit' => [
+                'name' => 'gst_save_settings',
+                'value' => 'Save GST Settings'
+            ]
+        ];
+
+    }
+
+    /**
+     * Method to render the form fields
+     * @param array
+     * @return string html contents
+     */
+    public static function render_fields(array $fields = []){
+
+        //Do not render anything at this point
+        if(empty($fields)){
+            return;
+        }
+
+        $content = '';
+
+        //Begin iterations
+        foreach($fields as $key => $field){
+
+            //Switch between key types
+            switch($key){
+                case 'text':
+                    $content .= '<label>' . $field['label'] . '</label><br>';
+                    $content .= '<input type="text" name="' . $field['name'] . '" value="' . $field['value'] . '"/>
+                        ' . ( (!empty($field['description'])) ? '<span class="description">' . $field['description'] . '</spa>' : '' ) . '
+                    <br>';
+                    break;
+                case 'checkbox':
+                    $content .= '<label>' . $field['label'] . '</label><br>';
+                    $content .= '<input type="checkbox" name="' . $field['name'] . '" ' . ( (!empty($field['value'])) ? 'checked' : '' ) . '/>
+                        ' . ( (!empty($field['description'])) ? '<span class="description">' . $field['description'] . '</spa>' : '' ) . '
+                    <br>';
+                    break;
+                case 'submit':
+                    $content .= '<input type="submit" class="button-primary" name="' . $field['name'] . '" value="' . $field['value'] . '"/>';
+                    break;
+
+            }
+
+        }
+
+        return $content;
+
+    }
+
 }
-GP_Admin_Menu::init();
+NPD_GST_Admin_Menu::init();
--- views/admin/settings.php
+++ views/admin/settings.php
@@ -7,23 +7,30 @@ defined('ABSPATH') or exit;
 <div class="wrap">
 
     <div id="tabs">
-        <h2 class="nav-tab-wrapper">
-            <a href="#tab-1" class="nav-tab">One Time Donation</a>
-            <a href="#tab-2" class="nav-tab">Recurring Donation</a>
-            <a href="#tab-3" class="nav-tab">My Account</a>
-        </h2>
 
-        <div id="tab-1" class="tab-content">
-            tab 1 content
-        </div>
+        <form method="post">
 
-        <div id="tab-2" class="tab-content">
-            tab 2 content
-        </div>
+            <h2 class="nav-tab-wrapper">
+                <?php
+                $counter_id = 1;
+                foreach($tabs as $key => $tab): ?>
 
-        <div id="tab-3" class="tab-content">
-            tab 3 content
-        </div>
+                <a href="#tab-<?php echo $counter_id; ?>" class="nav-tab"><?php echo $key; ?></a>
+
+                <?php $counter_id++; endforeach; ?>
+            </h2>
+
+            <?php
+                $counter_content = 1;
+                foreach($tabs as $tab): ?>
+
+                <div id="tab-<?php echo $counter_content; ?>" class="tab-content">
+                    <?php echo self::render_fields($tab); ?>
+                </div>
+
+            <?php $counter_content++; endforeach; ?>
+
+        </form>
         
     </div>
     
